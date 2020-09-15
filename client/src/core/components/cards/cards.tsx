5f33d08874d5787ce113966bdae68b878a0e8467
Tanvir-Kang <tanvir.kang1@gmail.com>
2020-09-14 21:29:28 -0700

    Cards Styling Completed

--- client/src/core/components/cards/cards.tsx
+++ client/src/core/components/cards/cards.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Card from 'react-bootstrap/Card'
 import Container from 'react-bootstrap/esm/Container';
 import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col'
+import Styles from './Styles'
 
 
 export interface ICardProps {
@@ -16,24 +18,26 @@ interface ICard {
 
 export const CardItem = (props: ICard) => {
   return (
-    
-    <Container>
-      <Row>
-      {props.services.map((item) => (
-        <Card style={{ width: '18rem' }}>
-          <Card.Body>
-        <Card.Title>{item.title}</Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">{item.subtitle}</Card.Subtitle>
-            {item.text.split('\n').map((itemText, i) => (
-              <Card.Text>{itemText}</Card.Text>
-            ))}
-            
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
-      </Card.Body>
-    </Card>
-      ))}
-        </Row>
-      </Container>
+
+    <Container fluid style={Styles.containerPadding}>
+      <Row className="justify-content-center">
+
+        {props.services.map((item) => (
+          <Card style={{ width: '20rem', margin: '1rem' }}>
+            <Card.Body>
+              <Card.Title>{item.title}</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">{item.subtitle}</Card.Subtitle>
+              {item.text.split('\n').map((itemText, i) => (
+                <Card.Text>{itemText}</Card.Text>
+              ))}
+
+              <Card.Link href="#">Card Link</Card.Link>
+              <Card.Link href="#">Another Link</Card.Link>
+            </Card.Body>
+          </Card>
+        ))}
+
+      </Row>
+    </Container>
   )
 }
--- client/src/pages/services/servicesPage.tsx
+++ client/src/pages/services/servicesPage.tsx
@@ -5,11 +5,11 @@ import { CardItem, ICardProps } from '../../core/components/cards/cards';
 
 var serviceList: ICardProps[] = [
   { title: "Service 1", text: "An intensive wash process consisting of: \n - Thorough-pre rinse \n - Tar, bug, sap removal ( if applicable ) \n- Foam bath \n - Hand wash \n- Detailed agitation of badges and trim \n- Hand dry \n- Tire scrub \n- Wheel decontamination \n- Tire shine \n- Door and trunk jambs", subtitle: "Express Wash" },
-  { title: "Service 2", text: "Express Wash", subtitle: "Wash and Wax" },
-  { title: "Service 3", text: "Express Wash", subtitle: "Paint Enhancement" },
-  { title: "Service 4", text: "Express Wash", subtitle: "Express Interior" },
-  { title: "Service 5", text: "Express Wash", subtitle: "Moderate Interior" },
-  { title: "Service 6", text: "Express Wash", subtitle: "Full Interior" },
+  { title: "Service 2", text: "- Express wash followed up by an application of gloss enhancing wax (4 month durability) \n (+) optional spray sealant: + $15 (6 month durability) \n (+) optional ceramic spray wax: + $20 (6+ month durability)", subtitle: "Wash and Wax" },
+  { title: "Service 3", text: "This package is intended for those who want to take it a notch above a wash. A paint enhancement will remove light swirls, scratches, and oxidation in the clear coat of the vehicle. An enhancement will restore gloss, increase clarity, and improves the condition of the paint. \n- Express wash (Service 1) \n- Complete paint decontamination \n- Machine polish \n- Paint protection", subtitle: "Paint Enhancement" },
+  { title: "Service 4", text: "A quick interior cleaning of the cabin and trunk \n- Vaccumm \n- Dust and debris removal \n- Glass cleaning \n- Door and trunk jambs", subtitle: "Express Interior" },
+  { title: "Service 5", text: "A deeper clean \n- Express interior (Service 4) \n- Dashboard, center console, doors, pillars are scrubbed and UV protection is applied \n- Leather seats are cleaned and conditioned", subtitle: "Moderate Interior" },
+  { title: "Service 6", text: "The deepest clean \n- Express + Moderate Interior (Service 4 + Service 5) \n- Full carpet steam clean, shampoo and extraction \n- Cloth seats are shampooed and extracted \n- Every interior surface is coated with protection", subtitle: "Full Interior" },
 ]
 
 export const Services = () => {
